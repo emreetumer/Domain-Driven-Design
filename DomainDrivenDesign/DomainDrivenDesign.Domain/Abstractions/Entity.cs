edcc7577de238571219f7e94ed5dac15d4984925
Emre Tümer <emreetumer0@gmail.com>
2026-08-09 16:16:47 +0300

    Implement value-based equality for Entity class
    Entity now implements IEquatable<Entity> with Equals(Entity? other) and overrides GetHashCode() to use Id. Removed unrelated code and updated Equals(object? obj) for consistency.

--- DomainDrivenDesign/DomainDrivenDesign.Domain/Abstractions/Entity.cs
+++ DomainDrivenDesign/DomainDrivenDesign.Domain/Abstractions/Entity.cs
@@ -1,6 +1,6 @@
 ﻿namespace DomainDrivenDesign.Domain.Abstractions;
 
-public abstract class Entity
+public abstract class Entity : IEquatable<Entity>
 {
     public Guid Id { get; init; } // init: Id elde edildikten sonra bir daha değiştirilememesini sağlar. 
     protected Entity(Guid id)
@@ -8,7 +8,6 @@ public abstract class Entity
         Id = id;
     }
 
-    // TestMethod methodunda id bazlı kontrol yapmamız için equals metodunu override ettik burada.
     public override bool Equals(object? obj)
     {
         if (obj == null)
@@ -28,26 +27,29 @@ public abstract class Entity
 
         return entity.Id == Id;
     }
-}
 
-#region SORUN => Id'ler burada eşit değil fakat biz eşit olmasını istiyoruz çünkü A sınıfı Entity'e bağlı.
-// Equals metodu burada ezmemiz gerekiyor equals bazlı değil de id bazlı kontrol olması lazım.
-public class A : Entity
-{
-    public A(Guid id) : base(id)
+    public override int GetHashCode()
     {
+        return Id.GetHashCode();
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other == null)
+        {
+            return false;
+        }
+
+        if (other is not Entity entity)
+        {
+            return false;
+        }
+
+        if (other.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
     }
 }
-
-public class Test
-{
-    public void TestMethod()
-    {
-        Guid id = Guid.NewGuid();
-        A a1 = new(id);
-        A a2 = new(id);
-
-        Console.WriteLine(a1.Equals(a2)); // equals override metodunu yazmasaydık burası false dönerdi.
-    }
-}
-#endregion
