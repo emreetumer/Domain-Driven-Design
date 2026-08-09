e004fc3fcb517562c714446db262e9401864cff2
Emre Tümer <emreetumer0@gmail.com>
2026-08-09 14:43:01 +0300

    Override Entity.Equals to compare by Id, not reference
    The Entity class now compares equality based on the Id property.
    Added sample and test classes to demonstrate that two Entity
    instances with the same Id are considered equal.

--- DomainDrivenDesign/DomainDrivenDesign.Domain/Abstractions/Entity.cs
+++ DomainDrivenDesign/DomainDrivenDesign.Domain/Abstractions/Entity.cs
@@ -7,4 +7,47 @@ public abstract class Entity
     {
         Id = id;
     }
+
+    // TestMethod methodunda id bazlı kontrol yapmamız için equals metodunu override ettik burada.
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if (obj is not Entity entity)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
 }
+
+#region SORUN => Id'ler burada eşit değil fakat biz eşit olmasını istiyoruz çünkü A sınıfı Entity'e bağlı.
+// Equals metodu burada ezmemiz gerekiyor equals bazlı değil de id bazlı kontrol olması lazım.
+public class A : Entity
+{
+    public A(Guid id) : base(id)
+    {
+    }
+}
+
+public class Test
+{
+    public void TestMethod()
+    {
+        Guid id = Guid.NewGuid();
+        A a1 = new(id);
+        A a2 = new(id);
+
+        Console.WriteLine(a1.Equals(a2)); // equals override metodunu yazmasaydık burası false dönerdi.
+    }
+}
+#endregion
