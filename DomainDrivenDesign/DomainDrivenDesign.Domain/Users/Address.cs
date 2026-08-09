fb41c5467ca482ad0a347017a6c5138e334657a4
Emre Tümer <emreetumer0@gmail.com>
2026-08-09 16:50:16 +0300

    Refactor User to use value objects for core fields
    User entity now uses Name, Email, Password, and Address value objects instead of primitive strings. Added validation logic to each value object and updated User methods and constructor to support these changes.

---
+++ DomainDrivenDesign/DomainDrivenDesign.Domain/Users/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace DomainDrivenDesign.Domain.Users;
+
+public sealed record Address(
+    string Country,
+    string City,
+    string Street,
+    string FullAddress,
+    string PostaCode);
---
+++ DomainDrivenDesign/DomainDrivenDesign.Domain/Users/Password.cs
@@ -0,0 +1,20 @@
+﻿namespace DomainDrivenDesign.Domain.Users;
+
+public sealed record Password
+{
+    public string Value { get; init; }
+    public Password(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentException("İsim alanı boş olamaz");
+        }
+
+        if (value.Length < 6)
+        {
+            throw new ArgumentException("İsim alanı 6 karakterden az olamaz!");
+        }
+
+        Value = value;
+    }
+}
