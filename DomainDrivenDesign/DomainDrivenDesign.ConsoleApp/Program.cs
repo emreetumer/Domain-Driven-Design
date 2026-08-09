2e7315c12e28ff1c3443003ac0b3542b1834fc18
Emre Tümer <emreetumer0@gmail.com>
2026-08-09 14:51:52 +0300

    Add console app demonstrating entity value equality
    Added DomainDrivenDesign.ConsoleApp (.NET 8) project. Implemented abstract Entity class with Id and value-based equality. Created class A inheriting from Entity. Program.cs demonstrates equality comparison of two A instances with the same Id.

---
+++ DomainDrivenDesign/DomainDrivenDesign.ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace DomainDrivenDesign.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Guid id = Guid.NewGuid();
+        A a1 = new(id);
+        A a2 = new(id);
+
+        Console.WriteLine(a1.Equals(a2));
+        Console.ReadLine();
+    }
+}
+
+public abstract class Entity
+{
+    public Guid Id { get; init; } // init: Id elde edildikten sonra bir daha değiştirilememesini sağlar. 
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    // maind methodunda id bazlı kontrol yapmamız için equals metodunu override ettik burada.
+    // Bu override methodu olmadan çalışırsa false döner. 
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if (obj is not Entity entity)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return entity.Id == Id;
+    }
+}
+
+public class A : Entity
+{
+    public A(Guid id) : base(id)
+    {
+    }
+}
