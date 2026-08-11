a3af48d5493e1cab10cbd782ac4d0773fac951b8
Emre Tümer <emreetumer0@gmail.com>
2026-08-11 10:29:50 +0300

    Domain Event 3
    Add MediatR and implement domain event handlers
    
    Added MediatR package for domain events. Introduced OrderDomainEvent and UserDomainEvent classes. Implemented handlers for order (email, SMS) and user registration (email) events.

---
+++ DomainDrivenDesign/DomainDrivenDesign.Domain/Orders/Events/SendOrderEmailEvent.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace DomainDrivenDesign.Domain.Orders.Events;
+
+public sealed class SendOrderEmailEvent : INotificationHandler<OrderDomainEvent>
+{
+    public Task Handle(OrderDomainEvent notification, CancellationToken cancellationToken)
+    {
+        // Mail gönderme işlemi
+        return Task.CompletedTask;
+    }
+}
---
+++ DomainDrivenDesign/DomainDrivenDesign.Domain/Users/Events/SendRegisterEmainEvent.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace DomainDrivenDesign.Domain.Users.Events;
+
+public sealed class SendRegisterEmainEvent : INotificationHandler<UserDomainEvent>
+{
+    public Task Handle(UserDomainEvent notification, CancellationToken cancellationToken)
+    {
+        //Kullanıcı için register email gönderme işlemi
+        return Task.CompletedTask;
+    }
+}
