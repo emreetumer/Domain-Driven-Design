0172daf4abe4b2ffd1702e111888197da588a73b
Emre Tümer <emreetumer0@gmail.com>
2026-08-11 17:53:46 +0300

    Add ASP.NET Core Web API project with MediatR controllers
    Added DomainDrivenDesign.WebApi project with config files and launch settings. Registered the project in the solution. Implemented Categories, Orders, Products, and Users controllers with Create and GetAll endpoints using MediatR. Updated DependencyInjection.cs to register handlers from Application and Domain. Configured services, controllers, Swagger, and middleware in Program.cs. Added sample HTTP request file for testing.

---
+++ DomainDrivenDesign/DomainDrivenDesign.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using DomainDrivenDesign.Application.Features.Products.CreateProduct;
+using DomainDrivenDesign.Application.Features.Products.GetAllProduct;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DomainDrivenDesign.WebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(request, cancellationToken);
+        return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(GetAllProductQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+}
