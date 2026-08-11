0969802e23d9ca8d4332347bac6a7cea33378b2e
Emre Tümer <emreetumer0@gmail.com>
2026-08-11 17:27:35 +0300

    Add Infrastructure project with EF Core and repos
    Added DomainDrivenDesign.Infrastructure with EF Core and Domain refs.
    Implemented ApplicationDbContext and entity mappings.
    Added repositories for User, Product, Category, and Order.
    Introduced DependencyInjection for DI registration.
    Updated solution file to include Infrastructure.
    Fixed typo in OrderStatusEnum.
    Set default OrderLines initialization in Order entity.

---
+++ DomainDrivenDesign/DomainDrivenDesign.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using DomainDrivenDesign.Domain.Users;
+using DomainDrivenDesign.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainDrivenDesign.Infrastructure.Repositories;
+
+internal class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> CreateUserAsync(string name, string password, string email, string country, string city, string street, string postaCode, string fullAddress, CancellationToken cancellationToken = default)
+    {
+        User user = User.CreateUser(
+            name,
+            email,
+            password,
+            country,
+            city,
+            street,
+            postaCode,
+            fullAddress);
+
+        await _context.Users.AddAsync(user);
+
+        return user;
+    }
+
+    public async Task<List<User>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.ToListAsync(cancellationToken);
+    }
+}
