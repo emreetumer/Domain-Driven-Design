f72fc3cd51f13dd23a183dd160a116d8580add2c
Emre Tümer <emreetumer0@gmail.com>
2026-08-11 18:01:50 +0300

    Bu adımı yapmassak Migration hatası alıyoruz.
    Add id-based constructors to entity classes for ORM support
    
    Added private constructors with Guid id to Category, Order, OrderLine, Product, and User entities. These constructors call the base Entity constructor and facilitate ORM entity materialization.

--- DomainDrivenDesign/DomainDrivenDesign.Domain/Orders/Order.cs
+++ DomainDrivenDesign/DomainDrivenDesign.Domain/Orders/Order.cs
@@ -5,6 +5,10 @@ namespace DomainDrivenDesign.Domain.Orders;
 
 public sealed class Order : Entity
 {
+    private Order(Guid id) : base(id)
+    {
+
+    }
 
     public Order(Guid id, string orderNumber, DateTime createdDate, OrderStatusEnum status) : base(id)
     {
